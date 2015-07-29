f075642ab2b5829606d22253939ab831306cd6d1
CanyonD <CanyonD8@gmail.com>
2015-07-30 01:43:10 +0300

    Initial commit

---
+++ src/VisualSound/VisualSound.java
@@ -0,0 +1,20 @@
+package VisualSound;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+
+/**
+ * Created by Green on 26.07.2015.
+ */
+public class VisualSound {
+    final static Logger logger = Logger.getLogger(VisualSound.class);
+
+    public static void main(String[] args) {
+        logger.info("Program start");
+
+ //       WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+
+
+    }
+}
